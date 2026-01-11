9aa6d011ad591072bf220ddb1dd039d6edbcbbfd
ZinHt9tHlaing <phyoearkar267@gmail.com>
2026-01-11 23:21:07 +0630

    SEO

--- client/src/pages/layout/Layout.tsx
+++ client/src/pages/layout/Layout.tsx
@@ -1,10 +1,25 @@
 import { Outlet } from "react-router";
 import Footer from "./Footer";
 import Header from "./Header";
+import { Helmet } from "react-helmet";
 
-const Layout = () => {
+interface Props {
+  title?: string;
+  description?: string;
+  keywords?: string;
+  author?: string;
+}
+
+const Layout = ({ title, description, keywords, author }: Props) => {
   return (
     <div className="flex min-h-screen flex-col overflow-hidden">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+        <title>{title}</title>
+      </Helmet>
       <Header />
       <main className="mt-5 flex-1 md:px-0 lg:px-20">
         <Outlet />
@@ -12,6 +27,13 @@ const Layout = () => {
       <Footer />
     </div>
   );
+};
+
+Layout.defaultProps = {
+  title: "Ecommerce app - shop now",
+  description: "mern stack project",
+  keywords: "react,node,express,mongodb",
+  author: "ZHH",
 };
 
 export default Layout;
